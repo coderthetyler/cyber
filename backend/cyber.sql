3f3b7312097bafc4d1bae78573314dd9de4b2d69
coderthetyler <cheery.home1669@fastmail.com>
2023-02-10 21:48:25 +0000

    Add initial SQL schema & mock up of some user endpoints

---
+++ backend/cyber.sql
@@ -0,0 +1,137 @@
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY,
+    username TEXT NOT NULL,
+    display_name TEXT NOT NULL,
+    password_hash TEXT NOT NULL,
+    creation_time INTEGER NOT NULL
+);
+
+CREATE TABLE failed_login_attempts (
+    id INTEGER PRIMARY KEY,
+    attempt_time INTEGER NOT NULL,
+    source_ip TEXT NOT NULL,
+    entered_username TEXT NOT NULL
+);
+
+CREATE TABLE successful_login_attempts (
+    id INTEGER PRIMARY KEY,
+    attempt_time INTEGER NOT NULL,
+    source_ip TEXT NOT NULL,
+    user_id INTEGER NOT NULL REFERENCES users(id)
+);
+
+CREATE TABLE buckets (
+    id INTEGER PRIMARY KEY,
+    display_name TEXT NOT NULL,
+    display_description TEXT NOT NULL
+);
+
+CREATE TABLE units (
+    id INTEGER PRIMARY KEY,
+    display_name TEXT NOT NULL,
+    symbol TEXT NOT NULL,
+    digits INTEGER NOT NULL
+);
+
+CREATE TABLE unit_exchange_rates (
+    id INTEGER PRIMARY KEY,
+    effective_time INTEGER NOT NULL,
+
+    source_unit_id INTEGER NOT NULL REFERENCES units(id),
+    source_unit_amount INTEGER NOT NULL,
+
+    target_unit_id INTEGER NOT NULL REFERENCES units(id),
+    target_unit_amount INTEGER NOT NULL
+);
+
+CREATE TABLE transfers (
+    id INTEGER PRIMARY KEY,
+    justification TEXT,
+
+    source_bucket_id INTEGER NOT NULL REFERENCES buckets(id),
+    source_unit_id INTEGER REFERENCES units(id),
+    source_bucket_amount INTEGER NOT NULL,
+    source_withdraw_time INTEGER NOT NULL,
+
+    target_bucket_id INTEGER NOT NULL REFERENCES buckets(id),
+    target_unit_id INTEGER REFERENCES units(id),
+    target_bucket_amount INTEGER NOT NULL,
+    target_deposit_time INTEGER NOT NULL
+);
+
+CREATE TABLE vendors (
+    id INTEGER PRIMARY KEY,
+    bucket_id INTEGER NOT NULL REFERENCES buckets(id)
+);
+
+CREATE TABLE jobs (
+    id INTEGER PRIMARY KEY,
+    company_name TEXT,
+    position_name TEXT,
+    position_description TEXT,
+    start_date TEXT NOT NULL,
+    end_date TEXT,
+    bucket_id INTEGER NOT NULL REFERENCES buckets(id)
+);
+
+CREATE TABLE paystubs (
+    id INTEGER PRIMARY KEY,
+    creation_time INTEGER NOT NULL,
+    from_date TEXT NOT NULL,
+    thru_date TEXT NOT NULL,
+    deposit_date TEXT NOT NULL,
+    job_id INTEGER NOT NULL REFERENCES jobs(id)
+);
+
+CREATE TABLE paystub_transfers (
+    id INTEGER PRIMARY KEY,
+    paystub_id INTEGER NOT NULL REFERENCES paystubs(id),
+    transfer_id INTEGER NOT NULL REFERENCES transfers(id)
+);
+
+CREATE TABLE paystub_attachments (
+    id INTEGER PRIMARY KEY,
+    upload_time INTEGER NOT NULL,
+    paystub_id INTEGER NOT NULL REFERENCES paystubs(id),
+    file_path TEXT NOT NULL
+);
+
+CREATE TABLE time_entries (
+    id INTEGER PRIMARY KEY,
+    creation_time INTEGER NOT NULL,
+    start_time INTEGER NOT NULL,
+    end_time INTEGER NOT NULL,
+    description TEXT NOT NULL,
+    job INTEGER REFERENCES jobs(id)
+);
+
+CREATE TABLE accounts (
+    id INTEGER PRIMARY KEY,
+    institute_name TEXT,
+    account_number INTEGER,
+    open_date TEXT NOT NULL,
+    close_date TEXT,
+    bucket_id INTEGER NOT NULL REFERENCES buckets(id)
+);
+
+CREATE TABLE statements (
+    id INTEGER PRIMARY KEY,
+    upload_time INTEGER NOT NULL,
+    issue_date TEXT NOT NULL,
+    from_date TEXT NOT NULL,
+    thru_date TEXT NOT NULL,
+    account_id INTEGER NOT NULL REFERENCES accounts(id)
+);
+
+CREATE TABLE statement_transfers (
+    id INTEGER PRIMARY KEY,
+    statement_id INTEGER NOT NULL REFERENCES statements(id),
+    transfer_id INTEGER NOT NULL REFERENCES transfers(id)
+);
+
+CREATE TABLE statement_attachments (
+    id INTEGER PRIMARY KEY,
+    upload_time INTEGER NOT NULL,
+    statement_id INTEGER NOT NULL REFERENCES statements(id),
+    file_path TEXT NOT NULL
+);
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -3,17 +3,33 @@ use axum::{
     response::IntoResponse,
     routing::{get, post},
     Json, Router,
+    extract::State,
 };
 use serde::{Deserialize, Serialize};
 use std::net::SocketAddr;
+use sqlx::sqlite::SqlitePool;
+
+#[derive(Clone)]
+struct AppState {
+    dbpool: SqlitePool,
+}
 
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
+    
+    tracing::debug!("connecting to db...");
+    let dbpool = SqlitePool::connect("sqlite:cyber.db").await.unwrap();
+    tracing::debug!("connected to db!");
+
+    let state = AppState {
+        dbpool,
+    };
 
     let app = Router::new()
-        .route("/", get(root))
-        .route("/users", post(create_user));
+        .route("/users/list", get(list_users))
+        .route("/users/create", post(create_user))
+        .with_state(state);
 
     let addr = SocketAddr::from(([0, 0, 0, 0], 8081));
     tracing::debug!("listening on {}", addr);
@@ -23,30 +39,50 @@ async fn main() {
         .unwrap();
 }
 
-async fn root() -> &'static str {
-    "Hello, World!"
+
+async fn list_users(State(state): State<AppState>) -> impl IntoResponse {
+    let users = sqlx::query_as::<_,User>(
+            "SELECT id, display_name, password_hash FROM users ORDER BY id")
+        .fetch_all(&state.dbpool)
+        .await
+        .unwrap();
+    (StatusCode::OK, Json(users))
 }
 
-async fn create_user(Json(payload): Json<CreateUser>) -> impl IntoResponse {
-    let user = User {
-        id: 1337,
-        username: payload.username,
-    };
-
-    // this will be converted into a JSON response
-    // with a status code of `201 Created`
+async fn create_user(State(state): State<AppState>, Json(payload): Json<CreateUser>) -> impl IntoResponse {
+    let mut conn = state.dbpool.acquire().await.unwrap();
+    let user_id = sqlx::query(
+            "INSERT INTO users ( display_name, password_hash ) VALUES ( ?1, ?2 )")
+        .bind(payload.display_name)
+        .bind(payload.password)
+        .execute(&mut conn)
+        .await
+        .unwrap()
+        .last_insert_rowid();
+    let user = sqlx::query_as::<_, User>(
+            "SELECT * FROM users WHERE id = ?1")
+        .bind(user_id)
+        .fetch_one(&mut conn)
+        .await
+        .unwrap();
     (StatusCode::CREATED, Json(user))
 }
 
-// the input to our `create_user` handler
-#[derive(Deserialize)]
-struct CreateUser {
-    username: String,
+
+#[derive(Serialize, sqlx::FromRow)]
+struct User {
+    id: i64,
+    display_name: String,
+    password_hash: String,
 }
 
-// the output to our `create_user` handler
 #[derive(Serialize)]
-struct User {
-    id: u64,
-    username: String,
+struct UserList {
+    user_list: Vec<User>,
+}
+
+#[derive(Deserialize)]
+struct CreateUser {
+    display_name: String,
+    password: String,
 }
